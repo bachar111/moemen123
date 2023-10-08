b4dcc0d2d989529a55612b509fc4545da865d0ca
bachar <bacharben111@gmail.com>
2023-10-08 16:40:11 +0200

    changing imports

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-export default {
+module.exports = {
   PORT: process.env.PORT || 5000,
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost:27017',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
