56e2205552ec14936c858521ded2d86cd1eec83f
bachar <bacharben111@gmail.com>
2023-10-08 18:42:45 +0200

    mongo

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://bachar:bachar@cluster0.jtrwfhl.mongodb.net/',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://bachar:bachar@cluster0.jtrwfhl.mongodb.net/?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
   accessKeyId: process.env.accessKeyId || 'accessKeyId',
